b9f81c602b1a4d071291dbc81a558dafe8ce733b
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2026-01-20 09:57:11 +0100

    ajout des températures maximales et minimales qui changenet de couleur <10 en bleu >25 en rouge et lever et coucher du soleil

--- meteo-app/src/components/Weather.js
+++ meteo-app/src/components/Weather.js
@@ -1,10 +1,18 @@
 import { useState, useEffect } from 'react';
 
+const formatTime = (timestamp) => {
+    if (!timestamp) return "";
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' });
+};
+
 function Meteo({ ville }) {
     const [meteo, setMeteo] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    
+   
 
     useEffect(() => {
         if (!ville) return;
@@ -14,7 +22,7 @@ function Meteo({ ville }) {
             setError(null);
             try {
                 let url;
-                // Si on a des coordonnées GPS (plus fiable)
+                // Si on a des coordonnées GPS 
                 if (ville.lat && ville.lon) {
                     url = `https://api.openweathermap.org/data/2.5/weather?lat=${ville.lat}&lon=${ville.lon}&appid=${API_KEY}&units=metric&lang=fr`;
                 } else {
@@ -41,6 +49,8 @@ function Meteo({ ville }) {
     if (error) return <p className="error">{error}</p>;
     if (!meteo) return null;
 
+    
+
     return (
         <div className="weather-card">
             <h2>{meteo.name}</h2>
@@ -49,10 +59,30 @@ function Meteo({ ville }) {
                 alt="icon" 
             />
             <p className="temp">{Math.round(meteo.main.temp)}°C</p>
+            <p className="feels-like">
+            Ressenti : {Math.round(meteo.main.feels_like)}°C
+            </p>
+            <div className="min-max-container">
+            {/* Si temp_min < 10, on écrit en bleu (#007bff), sinon en gris foncé */}
+                <span style={{ color: meteo.main.temp_min < 10 ? '#007bff' : '#444', fontWeight: 'bold' }}>
+                Min : {Math.round(meteo.main.temp_min)}°C
+            </span>
+    
+            <span className="separator">|</span>
+    
+            {/* Si temp_max > 25, on écrit en rouge (#ff4d4d), sinon en gris foncé */}
+                <span style={{ color: meteo.main.temp_max > 25 ? '#ff4d4d' : '#444', fontWeight: 'bold' }}>
+                Max : {Math.round(meteo.main.temp_max)}°C
+            </span>
+        </div>
             <p className="desc">{meteo.weather[0].description}</p>
             <div className="details">
                 <p>💧 Humidité : {meteo.main.humidity}%</p>
                 <p>💨 Vent : {meteo.wind.speed} km/h</p>
+            </div>
+            <div className="sun-times">
+                <p>🌅 Lever : {formatTime(meteo.sys.sunrise)}</p>
+                <p>🌇 Coucher : {formatTime(meteo.sys.sunset)}</p>
             </div>
         </div>
     );
